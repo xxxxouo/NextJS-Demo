555f579730949ac08f5c44c09c4bc7b2f083e091
洁神 <1041749499@qq.com>
2023-05-22 18:25:55 +0800

    监控网站分析

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function Home() {
   return (
@@ -108,6 +109,7 @@ export default function Home() {
           </p>
         </a>
       </div>
+      <Analytics />
     </main>
   )
 }
