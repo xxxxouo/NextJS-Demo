eca633836fd6df5bb737cba7dfe99cacc3b98f1d
洁神 <1041749499@qq.com>
2023-06-16 17:19:57 +0800

    控制台输出文字

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -6,6 +6,8 @@ export const metadata = {
   description: '小洁大王牛逼',
 }
 let blurDataURL = fs.readFileSync('src/asset/imgurl.txt','utf-8')
+console.log("%c欲买桂花同载酒,终不似少年游 \n金鳞岂是池中物,一遇风云便化龙","color:red;font-size:28px");
+  console.log(`%c访问另一个域名:${process.env.APP_ANOTHERURL}`,"");
 function Home() {
   return (
     <div className=' flex  flex-col items-center gap-2'>
--- src/app/template.jsx
+++ src/app/template.jsx
@@ -1,6 +1,5 @@
 export default function Template ({children}){
-  console.log("%c欲买桂花同载酒,终不似少年游 \n金鳞岂是池中物,一遇风云便化龙","color:red;font-size:28px");
-  console.log(`%c访问另一个域名:${process.env.APP_ANOTHERURL}`,"");
+  
   return (
     <div className="w-full">
        <p className=" text-gray-400 border-b border-red-400 border-solid text-center"> 我是根路由的 template </p>
