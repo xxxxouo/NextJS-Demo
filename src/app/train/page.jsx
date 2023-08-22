6109822db9b2e9a159fc9eff1f1ecaebe52563a8
xxxx-ouo <4854374@qq.com>
2023-08-22 16:36:23 +0800

    画中画

--- src/app/train/page.jsx
+++ src/app/train/page.jsx
@@ -4,6 +4,7 @@ import dynamic from "next/dynamic";
 import { useDpr } from "./hooks/useWindowApi";
 import Factory from "@/utils";
 import { flushSync } from "react-dom";
+import Link from "next/link"; 
 
 const DeadRender = dynamic(() => import("./components/DeadRender"));
 const ScrollList = dynamic(() => import("./components/UnlimitedList"));
@@ -16,6 +17,9 @@ function Page() {
     <div className=" mt-3">
       <DeadRender />
       <p>当前设备像素比为:{dpr}</p>
+      <Link className="p-2 rounded-md border border-solid border-red-400 inline-block" 
+         href={'./train/pictureInpicture'}
+      >进入画中画页面</Link>
       <CatchCanvas />
       <ScrollList />
     </div>
---
+++ src/app/train/pictureInpicture/page.jsx
@@ -0,0 +1,24 @@
+"use client"
+import React, { useEffect, useRef } from 'react'
+
+export default function page() {
+  const videoRef= useRef(null)
+  const onNext =  async ()=>{
+    if(videoRef.current){
+      await videoRef.current.requestPictureInPicture({
+        width: 480,
+        height: 270,
+      })
+    }
+  }
+  return (
+    <div className='flex flex-col justify-center items-center gap-5'>
+      <div id="playerContainer">
+        <div id="player">
+          <video ref={videoRef} src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4" id='video'></video>
+        </div>
+      </div>
+      <button className='p-3 border-solid border border-blue-400 rounded-lg' onClick={onNext}>打开画中画窗口!</button>
+    </div>
+  )
+}
