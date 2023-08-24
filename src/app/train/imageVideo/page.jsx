9d4a95555af10a0844c7b41824f4d17b6bc4d041
xxxx-ouo <4854374@qq.com>
2023-08-24 15:16:19 +0800

    文字

--- src/app/train/imageVideo/page.jsx
+++ src/app/train/imageVideo/page.jsx
@@ -4,8 +4,9 @@ import  createTextImage,{ TextImageOption } from 'text-to-image-video';
 
 export default function Page() {
   const videoRef = useRef(null);
+  const textRef = useRef(null);
   useEffect(()=>{
-    if(videoRef.current){
+    if(videoRef.current && textRef.current){
       createTextImage({
         canvas: videoRef.current,
         // 可选，配置作画的文本，默认为'6'
@@ -24,11 +25,32 @@ export default function Page() {
           height: 300,
         },
       })
+      createTextImage({
+        // 必填，配置canvas元素，最终作画在其上完成
+        canvas: textRef.current,
+        // 可选，配置作画的文本，默认为'6'
+        replaceText: '6',
+        // 可选，配置作画半径，该值越大越稀疏，默认为 10
+        raduis: 8,
+        // 可选，配置是否灰度化，若开启灰度化则会丢失色彩，默认为 false
+        isGray: false,
+        // 必填，配置作画内容
+        source: {
+          // 必填，配置画什么文本
+          text: '洁神牛逼',
+          // 选填，配置文本使用的字体，CSS 格式，默认为微软雅黑
+          fontFamily: 'Microsoft YaHei',
+          // 选填，配置文本尺寸，默认为 200
+          fontSize: 200,
+        },
+      });
     }
-  },[videoRef])
+  },[videoRef,textRef])
   return (
     <div>
       <canvas ref={videoRef} id="demo" />
+      <div className=' my-3'></div>
+      <canvas ref={textRef}  />
     </div>
   )
 }
