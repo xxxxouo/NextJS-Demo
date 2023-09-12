4a1457d825cf07c10c5d4447b8c1790a94c463f5
xxxx-ouo <4854374@qq.com>
2023-09-12 09:54:05 +0800

    narbar 滚动隐藏

--- src/app/template.jsx
+++ src/app/template.jsx
@@ -1,9 +1,9 @@
+
 export default function Template({ children }) {
   return (
     <div className="w-full">
       <p className=" text-gray-400 border-b border-red-400 border-solid text-center">
-        {" "}
-        我是根路由的 template{" "}
+        洁神出品 必出精品
       </p>
       {children}
     </div>
